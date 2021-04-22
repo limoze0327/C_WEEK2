01a7d0e15d42d2cf23f1c8930ec0ed483e81f60b
limoze0327 <81509483+limoze0327@users.noreply.github.com>
2021-04-23 02:20:14 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,30 +1,74 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-	int n, a, b;
-	cin >> n;
+	int n;
+	int a, b;
+	int x, y;
+	int m[3][10] = { 0 };
 
-	for (a = 1; a <= n * 2; a++) {
-		if (a % 2 == 0) {
-			for (b = 1; b <= n; b++) {
-				if (b % 2 == 0)
-					cout << "*";
-				else
-					cout << " ";
-			}
-			cout << endl;
+
+	while (1)
+	{
+		cout << "좌석을 예약하시겠습니까? (1 또는 0)" << endl;
+		cin >> n;
+
+		if (n == 0) {
+			cout << "좌석 예약 종료" << endl;
+			break;
 		}
-		else {
-			for (b = 1; b <= n; b++) {
-				if (b % 2 == 0)
-					cout << " ";
-				else
-					cout << "*";
+
+		else if (n == 1)
+		{
+			cout << "현재의 좌석은 다음과 같습니다." << endl;
+			cout << "-----------------------------------------------" << endl;
+			cout << "1 2 3 4 5 6 7 8 9 10" << endl;
+			cout << "-----------------------------------------------" << endl;
+
+			for (a = 0; a < 3; a++)
+			{
+				for (b = 0; b < 10; b++)
+				{
+					cout << m[a][b] << " ";
+				}
+				cout << endl;
 			}
-			cout << endl;
+		}
+
+		else
+		{
+			cout << "1번 또는 0번만 입력하세요." << endl;
+			
+		}
+
+		while (n == 1)
+		{
+			cout << "몇 열, 몇 번째 좌석을 예약하시겠습니까?" << endl;
+			cin >> x >> y;
+
+			if (m[x - 1][y - 1] == 0)
+			{
+				cout << "예약되었습니다." << endl;
+				m[x - 1][y - 1] = 1;
+				cout << "-----------------------------------------------" << endl;
+				cout << "1 2 3 4 5 6 7 8 9 10" << endl;
+				cout << "-----------------------------------------------" << endl;
+
+				for (a = 0; a < 3; a++)
+				{
+					for (b = 0; b < 10; b++)
+					{
+						cout << m[a][b] << " ";
+					}
+					cout << endl;
+				}
+			}
+
+			else if (m[x - 1][y - 1] == 1)
+			{
+				cout << "이미 예약된 자리입니다." << endl;
+			}
 		}
 	}
 }
-
