6844199f8815904b8cc5a128f05e7bf4ccc92bf4
limoze0327 <81509483+limoze0327@users.noreply.github.com>
2021-04-27 17:36:49 +0900

    Update 소스3.cpp

--- 소스3.cpp
+++ 소스3.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
 	int a = 10;
-	int* ptr;
-	ptr = &a;
+	int* ptr; // 포인터 선언을 위해서 변수 앞에 *을 붙임
+	ptr = &a; // 포인터 초기화를 위해서 변수 앞에 &를 붙임 
 
-	cout << "변수 a의 주소는 :" << &a << endl;
-	cout << "a의 값은 :" << a << endl;
+	cout << "변수 a의 주소는 :" << &a << endl; // 변수 a의 주소값을 출력
+	cout << "a의 값은 :" << a << endl; // 변수 a의 값을 출력
 
 }
